32383e1e053334362ade6839c8fa2e0d21b2ec44
datapike <datapike@gmail.com>
2016-04-01 17:39:31 +0000

    add destory friendship and show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,6 @@ class UsersController < ApplicationController
     @friendships = current_user.friends
   end
   
-  
-  
   def search
     @users = User.search(params[:search_param])
     
@@ -31,6 +29,11 @@ class UsersController < ApplicationController
     else
       redirect_to my_friends_path, flash[:error] = "There was ana error with adding user as friend"
     end
+  end
+  
+  def show
+    @user = User.find(params[:id])
+    @user_stocks = @user.stocks
   end
   
 end
